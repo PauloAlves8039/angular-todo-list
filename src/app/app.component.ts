cd1690323692c4187682c3ff9014679d697902c9
PauloAlves8039 <pj38alves@gmail.com>
2024-01-31 14:37:31 -0300

    Testando componente com dependência e signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TodoSignalsService } from './services/todo-signals.service';
+import { Component, EventEmitter, Input, Output, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
+import { Todo } from './models/model/todo.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, HeaderComponent, TodoCardComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   @Input() public projectName!: string;
   @Output() public outputEvent = new EventEmitter<string>();
 
-  title = 'todo-list';
+  public title = 'todo-list';
+  public todoSignal!: WritableSignal<Array<Todo>>;
+
+  constructor(private todoSignalsService: TodoSignalsService) {}
 
   public handleEmitEvent(): void {
     this.outputEvent.emit(this.projectName);
+  }
+
+  public handleCreateTodo(todo: Todo): void {
+    if (todo) {
+      this.todoSignalsService.updateTodos(todo);
+      this.todoSignal = this.todoSignalsService.todosState;
+    }
   }
 }
